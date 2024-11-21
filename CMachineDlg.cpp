4225ee3d5fada5451e3a7625ef081bcc724e8c07
Jeongtae <parkjt430@users.noreply.github.com>
2024-11-21 11:29:56 +0900

    백신 기계 클래스 새로 생성

---
+++ CMachineDlg.cpp
@@ -0,0 +1,34 @@
+﻿// CMachineDlg.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "ZombieGame.h"
+#include "afxdialogex.h"
+#include "CMachineDlg.h"
+
+
+// CMachineDlg 대화 상자
+
+IMPLEMENT_DYNAMIC(CMachineDlg, CDialogEx)
+
+CMachineDlg::CMachineDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DIALOG_MACHINE, pParent)
+{
+
+}
+
+CMachineDlg::~CMachineDlg()
+{
+}
+
+void CMachineDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CMachineDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CMachineDlg 메시지 처리기
---
+++ CMachineDlg.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "afxdialogex.h"
+
+
+// CMachineDlg 대화 상자
+
+class CMachineDlg : public CDialogEx
+{
+	DECLARE_DYNAMIC(CMachineDlg)
+
+public:
+	CMachineDlg(CWnd* pParent = nullptr);   // 표준 생성자입니다.
+	virtual ~CMachineDlg();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DIALOG_MACHINE };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
