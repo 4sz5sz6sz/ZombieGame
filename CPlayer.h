a02c87fd2bd6181a84d31b2ce4bdc56c7aa68605
4sz5sz6sz <1a2a3a9592@gmail.com>
2024-11-22 21:15:47 +0900

    체력 회복 속도 수정

--- CPlayer.h
+++ CPlayer.h
@@ -29,7 +29,7 @@ public:
     }
     
     void TakeDamage(double amount=5.0); // 데미지 입는 경우 호출
-    void Heal(double amount=0.1); // 회복하는 경우 호출
+    void Heal(double amount=0.5); // 회복하는 경우 호출
     bool IsAlive() const; // 살아있는지 검사. HP가 0 이상인지 여부.
 };
 
