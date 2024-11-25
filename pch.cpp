3a7862936641ab4fc8a26bd7994bf063ff76ddf7
4sz5sz6sz <1a2a3a9592@gmail.com>
2024-11-25 23:28:05 +0900

    pch.h에 클리어여부 저장하는 전역변수 추가 extern bool stageCleared[8];

--- pch.cpp
+++ pch.cpp
@@ -4,4 +4,4 @@
 
 // 미리 컴파일된 헤더를 사용하는 경우 컴파일이 성공하려면 이 소스 파일이 필요합니다.
 bool stageCleared[8] = { false, true, false, false, false, false, false, false }; // 초기화: 1단계만 해금
-//bool stageCleared[8] = { false, true, true, true, true, true, true, false}; // 디버깅용@@@@@ 올 클리어
+//bool stageCleared[8] = { false, true, true, true, true, true, true, false}; // 디버깅용@@@@ 올 클리어
